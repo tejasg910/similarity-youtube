005a660afc5c35e85f8689b193ac81ff10cf478d
Tejas <tejasgiri910@gmail.com>
2023-03-25 21:59:28 +0530

    completed login and register

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const ejs = require("ejs");
 console.log(__dirname);
 const expressEjsLayouts = require("express-ejs-layouts");
 app.use(express.static(path.join(__dirname, "/public")));
-app.get("/", (req, res) => {
-  res.render("home");
-});
 
 //set template engine
 
@@ -16,6 +13,20 @@ app.use(expressEjsLayouts);
 //seting views dir
 app.set("views", path.join(__dirname, "/resource/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 app.listen(8000, () => {
   console.log("server started on port ", PORT);
 });
